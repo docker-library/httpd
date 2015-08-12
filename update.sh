f9261633e074ac5fd9cb37270d33d950a518e183
Tianon Gravi <admwiggin@gmail.com>
2015-08-12 11:24:30 -0700

    Add initial .travis.yml

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
@@ -9,6 +9,8 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
+
+travisEnv=
 for version in "${versions[@]}"; do
 	fullVersion="$(curl -sSL --compressed "https://www.apache.org/dist/httpd/" | grep -E '<a href="httpd-'"$version"'[^"-]+.tar.bz2"' | sed -r 's!.*<a href="httpd-([^"-]+).tar.bz2".*!\1!' | sort -V | tail -1)"
 	(
@@ -17,4 +19,9 @@ for version in "${versions[@]}"; do
 			s/^(ENV HTTPD_VERSION) .*/\1 '"$fullVersion"'/;
 		' "$version/Dockerfile"
 	)
+	
+	travisEnv='\n  - VERSION='"$version$travisEnv"
 done
+
+travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
