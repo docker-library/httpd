12bf8c8883340c98b3988a7bade8ef2d0d6dcf8a
Tianon Gravi <admwiggin@gmail.com>
2016-07-06 22:17:08 -0700

    Sync Debian/Alpine more (simpler diff), and add SHA1 checking

--- update.sh
+++ update.sh
@@ -13,13 +13,18 @@ versions=( "${versions[@]%/}" )
 travisEnv=
 for version in "${versions[@]}"; do
 	fullVersion="$(curl -sSL --compressed "https://www.apache.org/dist/httpd/" | grep -E '<a href="httpd-'"$version"'[^"-]+.tar.bz2"' | sed -r 's!.*<a href="httpd-([^"-]+).tar.bz2".*!\1!' | sort -V | tail -1)"
+	sha1="$(curl -fsSL "https://www.apache.org/dist/httpd/httpd-$fullVersion.tar.bz2.sha1" | cut -d' ' -f1)"
 	(
 		set -x
-		sed -ri '
-			s/^(ENV HTTPD_VERSION) .*/\1 '"$fullVersion"'/;
-		' "$version/Dockerfile"
+		sed -ri \
+			-e 's/^(ENV HTTPD_VERSION) .*/\1 '"$fullVersion"'/' \
+			-e 's/^(ENV HTTPD_SHA1) .*/\1 '"$sha1"'/' \
+			"$version/Dockerfile" "$version"/*/Dockerfile
 	)
-	
+
+	for variant in alpine; do
+		travisEnv='\n  - VERSION='"$version VARIANT=$variant$travisEnv"
+	done
 	travisEnv='\n  - VERSION='"$version$travisEnv"
 done
 
