aa1a6b699929b59627d39a8940f2810709956cdf
Joe Ferguson <joe@infosiftr.com>
2017-01-10 11:49:40 -0800

    Fix http2 support on 2.4 in debian by using newer OpenSSL 1.0.2+

--- update.sh
+++ update.sh
@@ -10,6 +10,7 @@ fi
 versions=( "${versions[@]%/}" )
 
 nghttp2VersionDebian="$(docker run -i --rm debian:stretch-slim bash -c 'apt-get update -qq && apt-cache show "$@"' -- "libnghttp2-dev" |tac|tac| awk -F ': ' '$1 == "Version" { print $2; exit }')"
+opensslVersionDebian="$(docker run -i --rm debian:jessie-backports bash -c 'apt-get update -qq && apt-cache show "$@"' -- "openssl" |tac|tac| awk -F ': ' '$1 == "Version" { print $2; exit }')"
 
 travisEnv=
 for version in "${versions[@]}"; do
@@ -21,6 +22,7 @@ for version in "${versions[@]}"; do
 			-e 's/^(ENV HTTPD_VERSION) .*/\1 '"$fullVersion"'/' \
 			-e 's/^(ENV HTTPD_SHA1) .*/\1 '"$sha1"'/' \
 			-e 's/^(ENV NGHTTP2_VERSION) .*/\1 '"$nghttp2VersionDebian"'/' \
+			-e 's/^(ENV OPENSSL_VERSION) .*/\1 '"$opensslVersionDebian"'/' \
 			"$version/Dockerfile" "$version"/*/Dockerfile
 	)
 
